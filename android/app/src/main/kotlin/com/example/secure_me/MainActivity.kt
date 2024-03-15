84834c0953bd229e9eac43a90f3cdd048555e2e1
The-Black-Shadow <mehedihasan.cse18@gmail.com>
2024-03-15 15:42:24 +0600

    added stripe basics requirement

--- android/app/src/main/kotlin/com/example/secure_me/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/secure_me/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.secure_me
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
