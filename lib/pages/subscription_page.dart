ed7fa26e80ebd17081e17b048d50bbac864957de
The-Black-Shadow <mehedihasan.cse18@gmail.com>
2024-03-15 15:17:29 +0600

    extract purchase button and make the way to add stripe functionality

--- lib/pages/subscription_page.dart
+++ lib/pages/subscription_page.dart
@@ -42,6 +42,11 @@ class SubscriptionPackages extends StatelessWidget {
             final Map<String, dynamic> data =
                 documents[index].data() as Map<String, dynamic>;
 
+            TextEditingController brandController = TextEditingController();
+            TextEditingController modelController = TextEditingController();
+            TextEditingController regController = TextEditingController();
+            TextEditingController engineController = TextEditingController();
+
             return Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
@@ -62,125 +67,7 @@ class SubscriptionPackages extends StatelessWidget {
                     style: const TextStyle(
                         fontSize: 26), // Adjust the font size here
                   ),
-                  onTap: () {
-                    TextEditingController brandController =
-                        TextEditingController();
-                    TextEditingController modelController =
-                        TextEditingController();
-                    TextEditingController regController =
-                        TextEditingController();
-                    TextEditingController engineController =
-                        TextEditingController();
-
-                    final currentUser = FirebaseAuth.instance.currentUser;
-
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('Purchase ${data['packageName']}'),
-                        content: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              const SizedBox(height: 10),
-                              TextField(
-                                controller: brandController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Car Brand',
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Enter car brand',
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              TextField(
-                                controller: modelController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Car Model',
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Enter car model',
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              TextField(
-                                controller: regController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Car Register',
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Enter car register',
-                                ),
-                              ),
-                              const SizedBox(height: 10),
-                              TextField(
-                                controller: engineController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Car Engine',
-                                  border: OutlineInputBorder(),
-                                  hintText: 'Enter car engine',
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () async {
-                              String brand = brandController.text.trim();
-                              String model = modelController.text.trim();
-                              String reg = regController.text.trim();
-                              String engine = engineController.text.trim();
-
-                              if (brand.isNotEmpty &&
-                                  model.isNotEmpty &&
-                                  engine.isNotEmpty) {
-                                try {
-                                  // Calculate expiration date (1 year validity)
-                                  DateTime currentDate = DateTime.now();
-                                  DateTime expirationDate = currentDate.add(Duration(days: 365));
-
-                                  await FirebaseFirestore.instance
-                                      .collection('Subscriptions')
-                                      .doc(currentUser!.email)
-                                      .collection('Plans')
-                                      .add({
-                                    'Package': data['packageName'],
-                                    'Car Brand': brand,
-                                    'Car Model': model,
-                                    'Car Reg': reg,
-                                    'Car Engine': engine,
-                                    'Expiration Date': expirationDate,
-                                  });
-
-                                  brandController.clear();
-                                  modelController.clear();
-                                  regController.clear();
-                                  engineController.clear();
-
-                                  Navigator.pop(context);
-                                } catch (e) {
-                                  print('Error adding data: $e');
-                                }
-                              } else {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) => AlertDialog(
-                                    title: const Text('Error'),
-                                    content:
-                                        const Text('Please fill all fields.'),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () => Navigator.pop(context),
-                                        child: const Text('OK'),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              }
-                            },
-                            child: const Text('Purchase'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                  onTap: () => _purchasePackage(context, data, brandController, modelController, regController, engineController),
                 ),
               ),
             );
@@ -188,5 +75,156 @@ class SubscriptionPackages extends StatelessWidget {
         );
       },
     );
+  }
+
+  void _purchasePackage(BuildContext context, Map<String, dynamic> data,
+      TextEditingController brandController, TextEditingController modelController,
+      TextEditingController regController, TextEditingController engineController) async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Purchase ${data['packageName']}'),
+        content: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextField(
+                controller: brandController,
+                decoration: const InputDecoration(
+                  labelText: 'Car Brand',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter car brand',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: modelController,
+                decoration: const InputDecoration(
+                  labelText: 'Car Model',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter car model',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: regController,
+                decoration: const InputDecoration(
+                  labelText: 'Car Register',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter car register',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: engineController,
+                decoration: const InputDecoration(
+                  labelText: 'Car Engine',
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter car engine',
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              String brand = brandController.text.trim();
+              String model = modelController.text.trim();
+              String engine = engineController.text.trim();
+
+              if (brand.isNotEmpty && model.isNotEmpty && engine.isNotEmpty) {
+                _stripeCheckout(context, data, brandController, modelController, regController, engineController);
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Error'),
+                    content: const Text('Please fill all fields.'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
+            child: const Text('Purchase'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _stripeCheckout(BuildContext context, Map<String, dynamic> data,
+      TextEditingController brandController, TextEditingController modelController,
+      TextEditingController regController, TextEditingController engineController) async {
+    // Call the Stripe checkout functionality here
+    // Example:
+    try {
+      // Perform Stripe checkout
+      // This is a placeholder, you need to implement the actual Stripe checkout process
+      // For example:
+      // await StripePayment.createPaymentMethod()
+      // Handle payment success and call _handlePurchase
+      _handlePurchase(context, data, brandController, modelController, regController, engineController);
+    } catch (e) {
+      print('Error during Stripe checkout: $e');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Error'),
+          content: const Text('An error occurred during checkout. Please try again later.'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  void _handlePurchase(BuildContext context, Map<String, dynamic> data,
+      TextEditingController brandController, TextEditingController modelController,
+      TextEditingController regController, TextEditingController engineController) async {
+    String brand = brandController.text.trim();
+    String model = modelController.text.trim();
+    String reg = regController.text.trim();
+    String engine = engineController.text.trim();
+
+    try {
+      final currentUser = FirebaseAuth.instance.currentUser;
+      // Calculate expiration date (1 year validity)
+      DateTime currentDate = DateTime.now();
+      DateTime expirationDate = currentDate.add(Duration(days: 365));
+
+      await FirebaseFirestore.instance
+          .collection('Subscriptions')
+          .doc(currentUser!.email)
+          .collection('Plans')
+          .add({
+        'Package': data['packageName'],
+        'Car Brand': brand,
+        'Car Model': model,
+        'Car Reg': reg,
+        'Car Engine': engine,
+        'Expiration Date': expirationDate,
+      });
+
+      brandController.clear();
+      modelController.clear();
+      regController.clear();
+      engineController.clear();
+
+      Navigator.pop(context);
+    } catch (e) {
+      print('Error adding data: $e');
+    }
   }
 }
